2d07749b156ac3d9a721f76ba536d464b13f59c2
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-06-11 22:55:57 +0100

    Progress

--- js/main.js
+++ js/main.js
@@ -251,8 +251,7 @@ var wordflick = function () {
 };
 
 wordflick();
-
-var words ;
+var words = ['Check Out Some of My Works.'];
 var part,
     i = 0,
     offset = 0,
@@ -292,12 +291,59 @@ var wordflick = function () {
         offset--;
       }
     }
-    document.getElementsByClassName('class2')[0].innerText = part;
+    document.getElementsByClassName('class1')[0].innerText = part;
+  }, speed);
+};
+
+wordflick();
+
+var words = ['Love To Hear From You.'];
+var part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substring(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    document.getElementsByClassName('class1')[0].innerText = part;
   }, speed);
 };
 
 wordflick();
 	
+
   
 
    /*---------------------------------------------------- */
